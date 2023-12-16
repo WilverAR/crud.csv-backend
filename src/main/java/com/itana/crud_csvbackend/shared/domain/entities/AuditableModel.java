c1905dd06f2711db9d97190b3201726b25f58263
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-15 19:13:06 -0500

    feat(shared): add AuditableModel

---
+++ src/main/java/com/itana/crud_csvbackend/shared/domain/entities/AuditableModel.java
@@ -0,0 +1,20 @@
+package com.itana.crud_csvbackend.shared.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+
+@MappedSuperclass
+public class AuditableModel {
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+}
