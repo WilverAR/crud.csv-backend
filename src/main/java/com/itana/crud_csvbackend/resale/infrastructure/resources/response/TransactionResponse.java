b3805d55152de3d448518a029236992ad34e6d03
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-19 17:46:21 -0500

    fix: update application.yml and TransactionResponse

--- src/main/java/com/itana/crud_csvbackend/resale/infrastructure/resources/response/TransactionResponse.java
+++ src/main/java/com/itana/crud_csvbackend/resale/infrastructure/resources/response/TransactionResponse.java
@@ -1,7 +1,11 @@
 package com.itana.crud_csvbackend.resale.infrastructure.resources.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 
 @Getter
@@ -29,4 +33,10 @@ public class TransactionResponse {
     private String leaseCommenceDate;
 
     private Integer resalePrice;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updatedAt;
 }
